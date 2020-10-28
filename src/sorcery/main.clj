cd9768d1b95d292ce55d80c1d7e5155a43c40589
Oliver Caldwell <oliver.caldwell@viasat.com>
2020-10-28 19:25:27 +0000

    Example remote eval working

--- src/sorcery/main.clj
+++ src/sorcery/main.clj
@@ -1,7 +1,27 @@
-(ns sorcery.main)
+(ns sorcery.main
+  (:require [clojure.core.server :as server])
+  (:import [java.io PipedReader PipedWriter]))
 
 (defn add [a b]
   (+ a b))
 
+(defn remote-eval [code]
+  (with-open [reader (PipedReader.)
+              writer (PipedWriter.)]
+    (.connect reader writer)
+    (let [result! (promise)]
+      (future
+        (server/remote-prepl
+          "localhost" "5555"
+          reader
+          (fn [msg]
+            (deliver result! msg))))
+      (.write writer code)
+      (.flush writer)
+      @result!)))
+
 (defn -main []
   (println "Hello, World!" (add 1 2)))
+
+(comment
+  (remote-eval "(+ 1 2)"))
